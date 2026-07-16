19ffabf6bbf26661636156e0ed7cd5d258c0c3be
max1mka-dss <m.nadezhkin@garda.local>
2026-07-16 13:07:48 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import sync_playwright, expect
+
 with sync_playwright() as playwright:
 
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -19,8 +21,25 @@ with sync_playwright() as playwright:
     #.locator('button')
     registration_button.click()
 
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_be_visible()
-    expect(dashboard_header).to_have_text("Dashboard")
+    context.storage_state(path='browser-state.json')
 
-    #page.wait_for_timeout(4000)
+
+
+
+
+
+    #dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
+    #expect(dashboard_header).to_be_visible()
+    #expect(dashboard_header).to_have_text("Dashboard")
+
+   # page.wait_for_timeout(5000)
+with sync_playwright() as playwright:
+
+    browser = playwright.chromium.launch(headless=False)
+    #context = browser.new_context()
+    #page = browser.new_page()
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
