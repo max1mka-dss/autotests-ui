400d1deec765aa7c5b0c0149bef41c50a336a285
max1mka--dss <stebaslo04@gmail.com>
2026-07-30 23:49:44 +0300

    Знакомство с PageComponent

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -4,6 +4,8 @@ from playwright.sync_api import Page
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 @pytest.fixture
 def login_page(chromium_page:Page) -> LoginPage:
     return LoginPage(page=chromium_page)
@@ -16,3 +18,18 @@ def registration_page(chromium_page:Page) -> RegistrationPage:
 @pytest.fixture
 def dashboard_page(chromium_page:Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
+
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page_with_state)
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,7 +11,6 @@ def test_successful_registration(registration_page: RegistrationPage, dashboard_
         registration_page.fill_registration_form(email = 'user.name@gmail.com',username = 'username',password = 'password')
         registration_page.click_registration_button()
         dashboard_page.check_visible_dashboard_title()
-
         # chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
         #
         # email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
